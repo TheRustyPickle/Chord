f8b3553d6f9079f6e9804f49b4711e0dba654ccb
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-11-11 22:29:32 +0600

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,5 @@ pub mod parser;
 use parser::*;
 
 fn main() {
-    let data = "-cat category name -p -r admin, team member -ch channel-1 channel-2 channel-3".to_string();
-    println!("{:?}", parse_input(data));
+    
 }
