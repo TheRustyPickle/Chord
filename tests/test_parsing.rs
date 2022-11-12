2b2897bf7c5e1cb153cdc0c8aa2198df15556abb
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-11-12 21:31:30 +0600

    Test update

--- tests/test_parsing.rs
+++ tests/test_parsing.rs
@@ -1,6 +1,6 @@
 extern crate chord;
-use std::collections::HashMap;
 use chord::parse_input;
+use std::collections::HashMap;
 
 #[test]
 fn test_parse_1() {
@@ -9,8 +9,18 @@ fn test_parse_1() {
     let expected_data = HashMap::from([
         ("category", vec!["category name".to_string()]),
         ("private", vec!["true".to_string()]),
-        ("roles", vec!["admin".to_string(), "team member".to_string()]),
-        ("channels", vec!["channel-1 -r another role, third role".to_string(), "channel-2".to_string(), "channel-3".to_string()])
+        (
+            "roles",
+            vec!["admin".to_string(), "team member".to_string()],
+        ),
+        (
+            "channels",
+            vec![
+                "channel-1 -r another role, third role".to_string(),
+                "channel-2".to_string(),
+                "channel-3".to_string(),
+            ],
+        ),
     ]);
     let parsed_data = parse_input(data).unwrap();
 
@@ -23,9 +33,10 @@ fn test_parse_1() {
 
         let parsed_data = parse_input(format!("{data}")).unwrap();
 
-        let expected_data = HashMap::from([
-            ("roles", vec!["another role".to_string(), "third role".to_string()])
-        ]);
+        let expected_data = HashMap::from([(
+            "roles",
+            vec!["another role".to_string(), "third role".to_string()],
+        )]);
         assert_eq!(parsed_data, expected_data);
         break;
     }
