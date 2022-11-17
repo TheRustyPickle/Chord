7ab7bd80db5ab3ce84ff89862675712229c42bef
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-11-17 21:48:32 +0600

    Minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-pub mod parse;
-pub mod commands;
 pub mod bot;
+pub mod commands;
+pub mod parse;
 
+pub use bot::{
+    channel_data::{CategoryInfo, ChannelInfo},
+    start_bot,
+};
 pub use commands::*;
 pub use parse::parser;
-pub use bot::{start_bot, channel_data};
 
 pub async fn initialize() {
     start_bot().await;
