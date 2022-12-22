36b21b64626cb3ac25017786f9dd6d701a46dd4b
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-12-23 01:03:59 +0600

    Updated accept command

--- src/commands/accept.rs
+++ src/commands/accept.rs
@@ -1,9 +1,9 @@
-use crate::bot::{CategoryInfo, ChannelInfo};
+use crate::bot::ChannelInfo;
 use serenity::model::channel::ChannelType;
 use serenity::model::channel::{PermissionOverwrite, PermissionOverwriteType};
 use serenity::model::id::{ChannelId, GuildId, RoleId};
-use serenity::model::{ModelError, Permissions};
-use serenity::{prelude::*};
+use serenity::model::Permissions;
+use serenity::prelude::*;
 use std::collections::HashMap;
 use std::error::Error;
 use tracing::info;
@@ -15,17 +15,15 @@ pub async fn run(
 ) -> Result<(), Box<dyn Error>> {
     let mut all_category = HashMap::new();
     let all_roles = guild_id.roles(&ctx.http).await?;
-
     let mut everyone_role = None;
-    
+
     for (role_id, role) in &all_roles {
         if role.name == "@everyone" {
             everyone_role = Some(role_id)
         }
     }
-
+    
     let all_guild_channels = ctx.http.get_channels(guild_id.0).await?;
-
     for guild in all_guild_channels {
         if guild.kind == ChannelType::Category {
             all_category.insert(guild.name, guild.id);
@@ -36,44 +34,53 @@ pub async fn run(
         let category_id = match channel.get_category_name() {
             Some(name) => {
                 if all_category.contains_key(name) {
+                    info!("{name} Category name already exists. {}", all_category[name]);
                     Some(all_category[name])
                 } else {
-                    Some(
+                    let new_category = 
                         GuildId(guild_id.0)
                             .create_channel(&ctx.http, |c| {
                                 c.name(name).kind(ChannelType::Category);
-                                
+
                                 if channel.get_category_private() {
-                                    c.permissions(do_private_category(everyone_role.unwrap()));
+                                    c.permissions(do_private(everyone_role.unwrap()));
                                 }
                                 c
                             })
-                            .await?
-                            .id,
-                    )
+                            .await
+                            ?
+                            .id;
+                    all_category.insert(name.to_string(), new_category);
+                    info!("{name} Category does not exist. Creating new category. {new_category}");
+                    Some(new_category)
                 }
             }
             None => None,
         };
 
         if let Some(cat_id) = category_id {
+            let mut role_ids = Vec::new();
+
             if let Some(cat_roles) = channel.get_category_roles() {
-
-                let mut role_ids = Vec::new();
-
                 for (role_id, role) in all_roles.iter() {
-
                     if cat_roles.contains(&role.name) {
                         role_ids.push(role_id);
                     }
                 }
-                role_ids.push(everyone_role.unwrap());
+            }
 
-                override_permissions_public(cat_id, role_ids, &ctx).await?
+            if channel.get_category_private() {
+                override_permissions_private(cat_id, role_ids, &ctx)
+                    .await
+                    ?
+            } else {
+                override_permissions_public(cat_id, role_ids, &ctx)
+                    .await
+                    ?
             }
         }
 
-        let create_channel = GuildId(guild_id.0)
+        let created_channel = GuildId(guild_id.0)
             .create_channel(&ctx.http, |c| {
                 c.name(&channel.channel);
 
@@ -83,12 +90,55 @@ pub async fn run(
 
                 c
             })
-            .await.unwrap();
+            .await
+            ?;
+
+        let mut channel_roles = vec![];
+
+        if let Some(_) = channel.private {
+            created_channel
+                .id
+                .edit(&ctx.http, |c| {
+                    c.name(&channel.channel)
+                        .permissions(do_private(everyone_role.unwrap()))
+                })
+                .await?;
+        }
+
+        if let Some(ch_roles) = &channel.roles {
+            for (role_id, role) in all_roles.iter() {
+                if ch_roles.contains(&role.name) {
+                    channel_roles.push(role_id);
+                    info!("{} role found for channel", role.name);
+                }
+            }
+            if let Some(_) = channel.private {
+                override_permissions_private(
+                    created_channel.id,
+                    channel_roles,
+                    ctx,
+                )
+                .await
+                ?;
+            } else {
+                override_permissions_public(
+                    created_channel.id,
+                    channel_roles,
+                    ctx,
+                )
+                .await
+                ?;
+            }
+        }
     }
     Ok(())
 }
 
-async fn override_permissions_public(channel_id: ChannelId, roles: Vec<&RoleId>, ctx: &Context) -> Result<(), Box<dyn Error>> {
+async fn override_permissions_public(
+    channel_id: ChannelId,
+    roles: Vec<&RoleId>,
+    ctx: &Context,
+) -> Result<(), Box<dyn Error>> {
     let allow =
         Permissions::SEND_MESSAGES | Permissions::VIEW_CHANNEL | Permissions::READ_MESSAGE_HISTORY;
     let deny = Permissions::MENTION_EVERYONE
@@ -103,15 +153,22 @@ async fn override_permissions_public(channel_id: ChannelId, roles: Vec<&RoleId>,
         let overwrite = PermissionOverwrite {
             allow,
             deny,
-            kind: PermissionOverwriteType::Role(role.to_owned())
+            kind: PermissionOverwriteType::Role(role.to_owned()),
         };
-        channel_id.create_permission(&ctx.http, &overwrite).await?;
+        channel_id
+            .create_permission(&ctx.http, &overwrite)
+            .await
+            ?;
     }
 
     Ok(())
 }
 
-async fn override_permissions_private(channel_id: ChannelId, roles: Vec<&RoleId>, ctx: &Context) -> Result<(), Box<dyn Error>> {
+async fn override_permissions_private(
+    channel_id: ChannelId,
+    roles: Vec<&RoleId>,
+    ctx: &Context,
+) -> Result<(), Box<dyn Error>> {
     let allow =
         Permissions::SEND_MESSAGES | Permissions::VIEW_CHANNEL | Permissions::READ_MESSAGE_HISTORY;
     let deny = Permissions::empty();
@@ -120,21 +177,24 @@ async fn override_permissions_private(channel_id: ChannelId, roles: Vec<&RoleId>
         let overwrite = PermissionOverwrite {
             allow,
             deny,
-            kind: PermissionOverwriteType::Role(role.to_owned())
+            kind: PermissionOverwriteType::Role(role.to_owned()),
         };
-        channel_id.create_permission(&ctx.http, &overwrite).await?;
+        channel_id
+            .create_permission(&ctx.http, &overwrite)
+            .await
+            ?;
     }
 
     Ok(())
 }
 
-fn do_private_category(role: &RoleId) -> Vec<PermissionOverwrite> {
+fn do_private(role: &RoleId) -> Vec<PermissionOverwrite> {
     let allow = Permissions::empty();
     let deny = Permissions::VIEW_CHANNEL;
 
     vec![PermissionOverwrite {
         allow,
         deny,
-        kind: PermissionOverwriteType::Role(role.to_owned())
+        kind: PermissionOverwriteType::Role(role.to_owned()),
     }]
 }
