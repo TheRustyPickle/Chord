8cbe6cec8071ddc7b9c6ff126e391190f8238a1f
WaffleMixer <mohammad.sakib0194@gmail.com>
2023-01-05 21:12:19 +0600

    Fixed command name

--- src/commands/example.rs
+++ src/commands/example.rs
@@ -2,7 +2,7 @@ use serenity::builder::CreateApplicationCommand;
 use serenity::model::prelude::interaction::application_command::CommandDataOption;
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    command.name("help").description("This is a help command")
+    command.name("example").description("This is a help command")
 }
 
 pub fn run(_options: &[CommandDataOption]) -> String {
